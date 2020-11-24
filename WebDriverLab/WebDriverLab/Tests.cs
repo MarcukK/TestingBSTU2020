46687db5efa2e118aa8fef6016669cb4929dfa74
Marchuk Konstantin <marchuk2000@list.ru>
2020-11-24 18:44:29 +0300

    WebDriver Lab Added

--- WebDriverLab/WebDriverLab/Tests.cs
+++ WebDriverLab/WebDriverLab/Tests.cs
@@ -1,19 +1,25 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebDriverLab
 {
     class Tests
     {
+        string productNameForCheckingNameChangeOfClothingTest = "Medium Vintage Check Bonded Cotton Bum Bag";
 
         IWebDriver driver;
         string websiteUrl = "https://uk.burberry.com/logo-detail-econyl-sonny-bum-bag-p80256681?searchQuery=Logo%20Detail%20ECONYL%C2%AE%20Sonny%20Bum%20Bag";
+
+        private IWebDriver driverCheckingTheNameChangeOfTheSelectedClothing;
 
         [SetUp]
         public void SetupTests()
@@ -26,33 +32,68 @@ namespace WebDriverLab
 
         }
 
-
-        [Test]
-        public void Test1()
+        public static DefaultWait<IWebDriver> GetFluentWait(IWebDriver driver)
         {
-            driver.Navigate().GoToUrl(websiteUrl);
+            DefaultWait<IWebDriver> fluentWait = new DefaultWait<IWebDriver>(driver);
+            fluentWait.Timeout = TimeSpan.FromSeconds(50);
+            fluentWait.PollingInterval = TimeSpan.FromMilliseconds(250);
+            fluentWait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+            return fluentWait;
+        }
 
-            IWebElement colorChoice = driver.FindElement(By.CssSelector("div.swatch:nth-child(1)>a:nth-child(1)>span:nth-child(1)"));
-            colorChoice.Click();
+        public static IWebElement WaitForTheElement(DefaultWait<IWebDriver> fluentWait, By by)
+        {
+            while (true) //required to wait until element will be created(not showed, just created)
+            {
+                try
+                {
+                    IWebElement webElement = fluentWait.Until(x => x.FindElement(by));
+                    return webElement;
+                }
+                catch (Exception e)
+                { }
+            }
+        }
 
-            IWebElement cookiesPopup = driver.FindElement(By.CssSelector("#onetrust-accept-btn-handler"));
+
+        [Test, Description("Test is not complete")]
+        public void CheckingTheNameChangeOfTheSelectedClothing()
+        {
+            driverCheckingTheNameChangeOfTheSelectedClothing = driver;
+            
+            driverCheckingTheNameChangeOfTheSelectedClothing.Navigate().GoToUrl(websiteUrl);
+            
+            DefaultWait<IWebDriver> fluentWait = GetFluentWait(driverCheckingTheNameChangeOfTheSelectedClothing);
+
+
+            IWebElement cookiesPopup = WaitForTheElement(fluentWait, By.Id("onetrust-accept-btn-handler"));
             cookiesPopup.Click();
 
-            IWebElement addButton = driver.FindElement(By.CssSelector(".add-to-bag__button-title"));
+
+            IWebElement colorChoice = fluentWait.Until(x => x.FindElement(By.CssSelector("div.swatch:nth-child(1)>a:nth-child(1)>span:nth-child(1)")));
+            colorChoice.Click();
+
+            IWebElement productName = fluentWait.Until(x => x.FindElement(By.CssSelector("h1>span:nth-child(1)")));
+            while(productName.Text != productNameForCheckingNameChangeOfClothingTest) ;
+
+            IWebElement addButton = fluentWait.Until(x => x.FindElement(By.ClassName("add-to-bag__button-content")));
             addButton.Click();
 
-            IWebElement basket = driver.FindElement(By.CssSelector(".icon--glyph-bag>svg:nth-child(1)>path:nth-child(2)"));
+            IWebElement basketCounter = WaitForTheElement(fluentWait, By.ClassName("header__button--with-one-digit-counter"));
+            IWebElement basket = fluentWait.Until(x => x.FindElement(By.CssSelector("div.header__icon-container>ul>li:nth-child(3)")));
+            while (!basket.Displayed);
             basket.Click();
 
+            IWebElement productInBasketName = fluentWait.Until(x => x.FindElement(By.CssSelector("td.item-description.item-description-top.item-description-oneline.cell>h2")));
 
-
-            Assert.IsTrue(true);
+            Assert.AreEqual(productInBasketName.Text, productNameForCheckingNameChangeOfClothingTest);
         }
 
         [TearDown]
         public void TearDownTests()
         {
-            driver.Quit();
+            if (driver != null)
+                driver.Quit();
         }
 
     }
